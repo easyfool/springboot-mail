be3c5c3929990ae261bed5fe922b54eba30ed2de
wangfeng <wangfengbabe@163.com>
2019-10-13 19:19:47 +0800

    springboot mail

---
+++ src/main/java/com/github/wangfeng/springboot/mail/service/MailService.java
@@ -0,0 +1,35 @@
+package com.github.wangfeng.springboot.mail.service;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+public interface MailService {
+    /**
+     * 发送文本邮件
+     *
+     * @param to      收件人
+     * @param subject 主题
+     * @param content 内容
+     */
+    void sendSimpleMail(String to, String subject, String content);
+
+    /**
+     * 发送HTML邮件
+     *
+     * @param to      收件人
+     * @param subject 主题
+     * @param content 内容
+     */
+    void sendHtmlMail(String to, String subject, String content);
+
+
+    /**
+     * 发送带附件的邮件
+     *
+     * @param to       收件人
+     * @param subject  主题
+     * @param content  内容
+     * @param filePath 附件
+     */
+    void sendAttachmentsMail(String to, String subject, String content, String filePath);
+}
---
+++ src/test/java/com/github/wangfeng/springboot/mail/service/impl/MailServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.github.wangfeng.springboot.mail.service.impl;
+
+import com.github.wangfeng.springboot.mail.service.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MailServiceImplTest {
+    @Autowired
+    private MailService mailService;
+
+    /**
+     * 测试发送文本邮件
+     */
+    @Test
+    public void sendmail() {
+        mailService.sendSimpleMail("wangfengbabe@163.com", "主题：你好普通邮件", "内容：第一封邮件");
+    }
+
+    @Test
+    public void sendmailHtml() {
+        mailService.sendHtmlMail("wangfengbabe@163.com", "主题：你好html邮件", "<h1>内容：第一封html邮件</h1>");
+    }
+
+    @Test
+    public void sendMailAttachment() {
+        mailService.sendAttachmentsMail("wangfengbabe@163.com", "主题：你好普通邮件1", "内容：第一封邮件", "/Users/wangfeng/Documents/books/hello.txt");
+    }
+
+}
